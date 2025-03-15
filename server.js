539d9c61f880fa757d45f5f1c48149062453a579
artzamukie001 <sarawut_boon@elearning.cmu.ac.th>
2025-03-15 12:34:03 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+const cors = require("cors");
+
+// controllers
+const { UserController } = require("./controllers/UserController");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/api/user/signin", UserController.signIn);
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
