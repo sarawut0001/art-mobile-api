86bda8d4de6fa5af2b8982f4fa7c51baa0df51db
artzamukie001 <sarawut_boon@elearning.cmu.ac.th>
2025-03-15 17:38:49 +0700

    feat: create company controller and create api create company

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 // controllers
 const { UserController } = require("./controllers/UserController");
+const { CompanyController } = require("./controllers/CompanyController");
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +15,11 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+// User
 app.post("/api/user/signin", UserController.signIn);
+
+// Company
+app.post("/api/company/create", CompanyController.create);
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
